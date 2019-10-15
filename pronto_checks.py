186e16bf1fa811972765c8dc21d6b24c62781bf5
Kareha Agesa <agesak@uw.edu>
2019-10-15 15:27:02 -0700

    adding hw2

---
+++ pronto_checks.py
@@ -0,0 +1,33 @@
+from urllib.request import urlopen
+from io import StringIO
+import pandas as pd
+
+
+def read_in_data():
+
+    # read in pronto data
+    link = "https://data.seattle.gov/api/views/tw7j-dfaw/rows.csv?accessType=DOWNLOAD"
+    f = urlopen(link)
+    df = f.read()
+
+    # convert to dataframe
+    s=str(df,'utf-8')
+    data = StringIO(s) 
+    df = pd.read_csv(data)
+
+    return df
+
+def test_create_dataframe(df, cols):
+
+    conditions = []
+
+    # Does the dataframe contain only cols?
+    conditions.append(list(df) == cols)
+
+    # Do the values in each column have the same python type
+    conditions.append(len(df.dtypes.unique()) == 1)
+
+    # Are there at least 10 rows in the dataframe?
+    conditions.append(len(df) >= 10)
+
+    return all(conditions) == True
