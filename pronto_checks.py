1d3931d8e3780e69d24c9fb9e411ca55549c5d48
Kareha Agesa <agesak@uw.edu>
2019-10-17 20:31:07 -0700

    another way to do dtypes question

--- pronto_checks.py
+++ pronto_checks.py
@@ -11,11 +11,12 @@ def read_in_data():
     df = f.read()
 
     # convert to dataframe
-    s=str(df,'utf-8')
-    data = StringIO(s) 
+    s = str(df, 'utf-8')
+    data = StringIO(s)
     df = pd.read_csv(data)
 
     return df
+
 
 def test_create_dataframe(df, cols):
 
@@ -25,7 +26,14 @@ def test_create_dataframe(df, cols):
     conditions.append(list(df) == cols)
 
     # Do the values in each column have the same python type
-    conditions.append(len(df.dtypes.unique()) == 1)
+    # conditions.append(len(df.dtypes.unique()) == 1)
+
+    # or?
+    same_type = True
+    for col in list(df):
+        if len(df[col].dtype) != 0:
+            same_type = False
+    conditions.append(same_type)
 
     # Are there at least 10 rows in the dataframe?
     conditions.append(len(df) >= 10)
