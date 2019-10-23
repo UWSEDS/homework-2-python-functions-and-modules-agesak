9ee72273da290074ddc7e1aafd4110f5e32588a2
Kareha Agesa <agesak@uw.edu>
2019-10-22 21:23:43 -0700

    one more edit for question 2

--- pronto_checks.py
+++ pronto_checks.py
@@ -13,12 +13,15 @@ def test_create_dataframe(df, cols):
     # Does the dataframe contain only cols?
     conditions.append(list(df) == cols)
 
-    # Do the values in each column have the same python type
-    same_type = True
+    # Do the values in each column have the same python type?
+    col_types = []
     for col in list(df):
-        if len(df[col].dtype) != 0:
-            same_type = False
-    conditions.append(same_type)
+        types = []
+        for index, row in df[col].iteritems():
+            types.append(type(row))
+        types = set(types)    
+        col_types.append(len(types)==1)
+    conditions.append(col_types)
 
     # Are there at least 10 rows in the dataframe?
     conditions.append(len(df) >= 10)
